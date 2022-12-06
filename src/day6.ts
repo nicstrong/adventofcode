b43e6c9b8a0fc41639ed4a8fb8ccfe50279dc369
Nic Strong <nic.strong@ipfx.com>
2022-12-06 18:42:41 +1300

    Day 6 checkin

---
+++ src/day6.ts
@@ -0,0 +1,37 @@
+import { readLines } from './util/util.js'
+
+export async function day6() {
+    var data = await readLines('data/day6.data')
+
+    console.log(`part1 1: ${startMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4)}`)
+    console.log(`part1 2: ${startMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)}`)
+    console.log(`part1 3: ${startMarker('nppdvjthqldpwncqszvftbrmjlhg', 4)}`)
+    console.log(`part1 4: ${startMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)}`)
+    console.log(`part1 5: ${startMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)}`)
+    console.log(`part1 answer: ${startMarker(data[0]!,4 )}`)
+    console.log()
+    console.log(`part2 1: ${startMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14)}`)
+    console.log(`part2 2: ${startMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 14)}`)
+    console.log(`part2 3: ${startMarker('nppdvjthqldpwncqszvftbrmjlhg', 14)}`)
+    console.log(`part2 4: ${startMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14)}`)
+    console.log(`part2 5: ${startMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14)}`)
+    console.log(`part2 answer: ${startMarker(data[0]!, 14)}`)
+}
+
+
+
+
+function startMarker(input: string, len: number) {
+    function isUnique(str: string) {
+        return new Set(str).size == str.length;
+    }
+
+    for (let i = 0; i < input.length; ++i) {
+        const next = input.substring(i, i + len)
+        if (isUnique(next)) {
+            return i + len
+        }
+
+    }
+    return -1
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { day5 } from "./day5.js"
+import { day6 } from "./day6.js"
 
 // day1()
 // day2()
 // day3()
 // day4()
-day5()
+// day5()
+day6()
