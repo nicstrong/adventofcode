560369adce15d827b5534edf27b730b70c695e26
Nic Strong <nic.strong@ipfx.com>
2022-12-14 22:41:08 +1300

    DAy 14 prep

---
+++ src/2022/day14.ts
@@ -0,0 +1,11 @@
+import { readAocData } from '../util/util.js'
+const DATA_FILE = 'data/2022/day14_sample.in'
+
+export async function day14() {
+    
+}
+
+
+async function getInput() {
+    var data = await readAocData(DATA_FILE)
+}
